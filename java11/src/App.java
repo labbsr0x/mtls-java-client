ae268c14bcf51b9a35fb15d1077c14bb5825c32d
tiagostutz <tiagostutz@gmail.com>
2021-03-18 17:20:58 -0300

    feat: working example with mTLS server sidecar.labbs.com.br

--- java11/src/App.java
+++ java11/src/App.java
@@ -6,10 +6,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Java 11");
 
-        var clientCertificatePEM = "-----BEGIN CERTIFICATE-----\nMIIBnjCCAUSgAwIBAgIUFONqtKB9w4E3iMUHv94n03hGy9swCgYIKoZIzj0EAwIw\nEzERMA8GA1UEAxMIbXRscy5kZXYwHhcNMjEwMzE3MTc0NTAwWhcNMjIwMzE3MTc0\nNTAwWjAUMRIwEAYDVQQDEwlsb2NhbGhvc3QwWTATBgcqhkjOPQIBBggqhkjOPQMB\nBwNCAATtsHTYeVE705oaoXaiZ64/nTJ9l7k9eCA3Y58BgbcUleYzJtxAztbbF1xK\nidZUbDdECVtNHpzo+lnEUo/KEy7Yo3UwczAOBgNVHQ8BAf8EBAMCBaAwEwYDVR0l\nBAwwCgYIKwYBBQUHAwIwDAYDVR0TAQH/BAIwADAdBgNVHQ4EFgQUfmVic1TWBX4+\nZ3gw4v5ZnYH2mE4wHwYDVR0jBBgwFoAU0iIAebLrjpXUxqUXD4p8Xm1TAbUwCgYI\nKoZIzj0EAwIDSAAwRQIgX4pZ96+2Xnu/YkCI2cOTMY2VrEyPKaikS7WWOPsQztIC\nIQDHA8GfJitFy/U7CRFKf6IKUDvHmbDbDlwJX9XBoDOOVQ==\n-----END CERTIFICATE-----";
-        var clientPrivateKeyPEM = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQglrsSXTZk9EL6wWkg\n70WRPhlRJkN0dI2jKMnITbzlWr6hRANCAATtsHTYeVE705oaoXaiZ64/nTJ9l7k9\neCA3Y58BgbcUleYzJtxAztbbF1xKidZUbDdECVtNHpzo+lnEUo/KEy7Y\n-----END PRIVATE KEY-----";
-        var clientePrivateKeyPassphrase = "";
-        var serverCA = "-----BEGIN CERTIFICATE-----\nMIIBwDCCAWagAwIBAgIUMMN7D/SMJN0G1MTNBtMabI1WGSEwCgYIKoZIzj0EAwIw\nEzERMA8GA1UEAxMIbXRscy5kZXYwHhcNMjEwMzE3MTc0NTAwWhcNMjIwMzE3MTc0\nNTAwWjAUMRIwEAYDVQQDEwlsb2NhbGhvc3QwWTATBgcqhkjOPQIBBggqhkjOPQMB\nBwNCAARa/qfz8XDD88fKqy3XTeOIjyDt5HWVNNQXeTaAKpyjiuG2PCu0wtZgt8JU\nlDwDNu0qfZlB/mqsflH8x+3Om9/9o4GWMIGTMA4GA1UdDwEB/wQEAwIFoDATBgNV\nHSUEDDAKBggrBgEFBQcDATAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBTeEvt8HSyn\nq04FNBqPS/tXhBh+czAfBgNVHSMEGDAWgBTSIgB5suuOldTGpRcPinxebVMBtTAe\nBgNVHREEFzAVgglsb2NhbGhvc3SCCG10bHMuZGV2MAoGCCqGSM49BAMCA0gAMEUC\nIQDRcT/RBc+kQMkpr7GFOD6PR0L2Rm/C3uIEsHVsgJiTuwIgWy4vaQO0/V//BM1m\nRqSDzUrTeSUc24s6aivBM7Y+STU=\n-----END CERTIFICATE-----";
+        String clientCertificatePEM = "-----BEGIN CERTIFICATE-----\nMIIBnTCCAUKgAwIBAgIUChyT2Cfn6UzQ46O3z8j9zo/vr5gwCgYIKoZIzj0EAwIw\nEzERMA8GA1UEAxMIbXRscy5kZXYwHhcNMjEwMzE4MTgwMzAwWhcNMjIwMzE4MTgw\nMzAwWjASMRAwDgYDVQQDEwdTYW5kbWFuMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcD\nQgAEA8B5onZvmwT0Lsnxo4vbflKF79MP4xw0GvQa8TX6M675Uk09FTsqWxSdJGOb\n1ZLC6rzwa5DUblYBjCR6q0sVF6N1MHMwDgYDVR0PAQH/BAQDAgWgMBMGA1UdJQQM\nMAoGCCsGAQUFBwMCMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFP6Z0m7+ey59Aq12\npH73hyBJyTHtMB8GA1UdIwQYMBaAFAGdIruEIxp1V0wffo9YoK62HtvVMAoGCCqG\nSM49BAMCA0kAMEYCIQCgYv4A1Gq3f9DTNY3CUO0DP3LiMRZ/8fTbh1/g3Mm34AIh\nAMmzEkSMNzBDr99WNls0t0cZJWHW8c2844uxJcEbCNjN\n-----END CERTIFICATE-----";
+        String clientPrivateKeyPEM = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgT1k7IzW/nRg4JoLq\nGdnsjzyqTkD3A58/tgAQ1WWKA3WhRANCAAQDwHmidm+bBPQuyfGji9t+UoXv0w/j\nHDQa9BrxNfozrvlSTT0VOypbFJ0kY5vVksLqvPBrkNRuVgGMJHqrSxUX\n-----END PRIVATE KEY-----";
+        String clientePrivateKeyPassphrase = "";
+        String serverCertificate = "-----BEGIN CERTIFICATE-----\nMIIB4DCCAYegAwIBAgIUDJOyF5i6qWe6nF3tWZ9qGgLnCyMwCgYIKoZIzj0EAwIw\nEzERMA8GA1UEAxMIbXRscy5kZXYwHhcNMjEwMzE4MTgwMzAwWhcNMjIwMzE4MTgw\nMzAwWjAkMSIwIAYDVQQDExlzaWRlY2FyLm10bHMubGFiYnMuY29tLmJyMFkwEwYH\nKoZIzj0CAQYIKoZIzj0DAQcDQgAE5i9g8JuZljybB1h082MAHMdAEuJAl2K634hK\nwrCRhtMu+JmUB5ygG46TZBV6nDcY9uUuLkOocy+KsIiSzG0+3qOBpzCBpDAOBgNV\nHQ8BAf8EBAMCBaAwEwYDVR0lBAwwCgYIKwYBBQUHAwEwDAYDVR0TAQH/BAIwADAd\nBgNVHQ4EFgQURAtvgTT3/GY2vV1e4etWL4fCV38wHwYDVR0jBBgwFoAUAZ0iu4Qj\nGnVXTB9+j1igrrYe29UwLwYDVR0RBCgwJoIJbG9jYWxob3N0ghlzaWRlY2FyLm10\nbHMubGFiYnMuY29tLmJyMAoGCCqGSM49BAMCA0cAMEQCIF3EO8rYG+gP/XlptMmf\nLypVK0Rxqdm92wn6v1Xu9xzfAiB/TwMmJB5sA7yPboshaj/FweBSxntsC4QC5Lgl\n3eZUrQ==\n-----END CERTIFICATE-----";
 
         String url = "";
         if (args.length > 0 && args[0].contains("--url")) {
@@ -21,7 +21,7 @@ public class App {
         boolean localhostEnabled = args.length > 1 && args[1].equalsIgnoreCase("--localhostEnabled");
         System.out.println("Target Endpoint:" + url);
         HttpResponse<String> resp = MTLSHttpClient.doRequest(url, clientCertificatePEM, clientPrivateKeyPEM,
-                clientePrivateKeyPassphrase, serverCA, localhostEnabled);
+                clientePrivateKeyPassphrase, serverCertificate, localhostEnabled);
 
         System.out.println(resp.body());
     }
